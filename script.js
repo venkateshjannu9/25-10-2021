f3bbced0474f5e21c08bf678f5c6337dd2b487b2
venkateshjannu9 <91652197+venkateshjannu9@users.noreply.github.com>
2021-10-25 20:33:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent= `🎉Correct Number!`;
+
+document.querySelector('number').textContent = 13;
+document.querySelector('.score') .textContent= 20;
+(document.querySelector('.guess').value)
+document.querySelector('.guess').value=23;
+
+const x = function (){
+    console.log(23);
+}
+
+document.querySelector('check').addEventListener('click',function(){
+    console.log(documnet.querySelector('.guess').value);
+    console.log( guess,typeof guess);
+    if(!guess){
+        document.querySelector('.message').textContent = 'No number!';   
+    }
+});
